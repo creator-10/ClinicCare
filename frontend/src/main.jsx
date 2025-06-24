398eeef62e96652c858be4a62f7566866fed8fc8
akhilaakhi27 <akhilaaravind99@gmail.com>
2025-06-24 19:47:52 +0530

    main file updated

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,22 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-<<<<<<< HEAD
-import { BrowserRouter } from 'react-router-dom'
-//import AppContextProvider from './context/AppContext.jsx'
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter>
-
-    {/* <AppContextProvider> */}
-     
-     <App />
-
-   {/* </AppContextProvider> */}
-    </BrowserRouter>
-   
-  </StrictMode>,
-=======
 import  {Provider} from 'react-redux'
 import { store } from './redux/store.js'
 import { BrowserRouter } from 'react-router-dom'
@@ -32,5 +16,4 @@ createRoot(document.getElementById('root')).render(
     </BrowserRouter>
   </StrictMode>
  
->>>>>>> 51a0b6f (first)
 )
