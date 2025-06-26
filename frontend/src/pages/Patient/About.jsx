a1854915f5d6cec863ade42668a9bcb8903b9b00
Miss. Sneha Vasudevan <snehaavasudevan@gmail.com>
2025-06-27 00:22:17 +0530

    added About.jsx

---
+++ frontend/src/pages/Patient/About.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const About = () => {
+  return (
+    <div>
+      <div className='text-center text-2xl pt-10 text-gray-500'>
+      <p>ABOUT<span className='text-gray-700 font-medium'> US</span></p>
+      </div>
+
+      <div className='my-10 flex flex-col md:flex-row gap-12'>
+        <img className='w-full md:max-w-[360px]' src={assets.about} alt="" />
+        <div className='flex flex-col justify-center gap-6 md:w-2/4 text-sm text-gray-600'>
+          <p>Medico is a modern healthcare platform designed to simplify the way patients connect with trusted medical professionals.</p>
+          <p>Whether you need a quick consultation or a specialized diagnosis, Medico brings together top-rated doctors from various fields, all in one place. With a seamless interface, secure booking, and verified practitioner profiles, Medico ensures that accessing quality care is as easy as a few clicks. From general physicians to highly trained specialists, Medico makes healthcare more accessible, transparent, and patient-focused.</p>
+          <b className='text-gray-800'>Our Mission</b>
+          <p>What sets Medico apart is its extensive coverage of medical specialities and its commitment to user convenience. Patients can explore a wide range of departments like cardiology, dermatology, pediatrics, orthopedics, and more — each curated with verified doctors and real-time availability. Whether you're booking a routine checkup or a specialist appointment, Medico's intuitive platform offers personalized results and trusted options tailored to your health needs. It's not just a booking tool — it's a complete care connector.</p>
+
+        </div>
+      </div>
+
+<div className='text-xl my-4'>
+  <p>WHY <span className='text-gray-700 font-semibold'>CHOOSE US</span></p>
+</div>
+<div className='flex flex-col md:flex-row mb-20'>
+  <div className='border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-primary hover:text-white transition-all duration-300 text-gray-600 cursor-pointer'>
+    <b>Effectiveness</b>
+    <p>Medico streamlines the healthcare journey with fast, reliable appointment booking.
+Trusted by patients and doctors alike, it delivers care when and where it’s needed.</p>
+  </div>
+  <div className='border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-primary hover:text-white transition-all duration-300 text-gray-600 cursor-pointer'>
+    <b>Accessibility</b>
+    <p>Medico makes quality healthcare accessible to everyone, anytime, anywhere.
+From your phone to your doorstep, care is just a tap away.</p>
+  </div>
+  <div className='border px-10 md:px-16 py-8 sm:py-16 flex flex-col gap-5 text-[15px] hover:bg-primary hover:text-white transition-all duration-300 text-gray-600 cursor-pointer'>
+    <b>Health aide</b>
+    <p>Medico’s dedicated health aides are here to assist you every step of the way.
+From routine checkups to ongoing care, we ensure comfort and compassion.</p>
+  </div>
+</div>
+
+    </div>
+  )
+}
+
+export default About
