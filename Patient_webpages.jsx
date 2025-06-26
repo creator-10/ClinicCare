81c4f33cd3279ffa3709520bc03557cadcc16dcc
Miss. Sneha Vasudevan <snehaavasudevan@gmail.com>
2025-06-26 20:56:25 +0530

    Create Patient_webpages.jsx

---
+++ Patient_webpages.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from '../components/Header';
+import SpecialityMenu from '../components/SpecialityMenu';
+import Scheduleappointment from '../components/Scheduleappointment';
+
+const Patient_webpage = () => {
+  return (
+    <div>
+      
+      <Header />
+      <SpecialityMenu />
+      <Scheduleappointment />
+     
+    </div>
+  )
+}
+
+export default Patient_webpage
