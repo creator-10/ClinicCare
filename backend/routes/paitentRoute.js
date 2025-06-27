1bcefdf519ee536bc56178422e1d78a0665c3709
Miss. Sneha Vasudevan <snehaavasudevan@gmail.com>
2025-06-27 08:54:08 +0530

    Update paitentRoute.js

--- backend/routes/paitentRoute.js
+++ backend/routes/paitentRoute.js
@@ -1,8 +1,7 @@
 // here filename given patientRoute.js but in system routes/userRoute.js
 import express from 'express';
 import { registerUser, loginUser, profileUser } from '../controllers/userController.js';
-import protect from '../middleware/authMiddleware.js'; // this path must point to the correct file
-
+import protect from '../middleware/authMiddleware.js'; 
 const router = express.Router();
 
 router.post('/register', registerUser);
