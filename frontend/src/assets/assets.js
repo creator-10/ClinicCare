dc742f1c029404423c41b4247ec0a1f6ae5ebf8b
Miss. Sneha Vasudevan <snehaavasudevan@gmail.com>
2025-06-27 08:40:57 +0530

    check updated assets.js

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,66 @@
+import register from './p_register.jpg';
+import General_physician from './General_physician.png';
+import logo from './logo.png';
+import profile_pic from './profile_pic.jpg';
+import dropdown_icon from './dropdown_icon.jpg';
+import arrow_icon from './arrow_icon.jpg';
+import group_profiles from './group_profiles.png';
+import header_img from './header_img.png';
+import Dermatology from './dermatology.png';
+import Radiology from './radiology.png';
+import Orthopedics from './ortopedics.png';
+import Pediatrics from './pediatrics.png';
+import Cardiology from './cardiology.png';
+import about from './about.jpg';
+import contact from './contact.jpg';
+import cross_icon from './cross_icon.jpg';
+import logo1 from './logo1.jpg';
+import appointment from './appointment.jpg';
+
+export const assets = {
+  register,
+  General_physician,
+  logo,
+  logo1,
+  cross_icon,
+  profile_pic,
+  dropdown_icon,
+  arrow_icon,
+  group_profiles,
+  header_img,
+  Dermatology,
+  Radiology,
+  Orthopedics,
+  Pediatrics,
+  Cardiology,
+  about,
+  contact,
+  appointment,
+};
+
+export const specialityData = [
+  {
+    speciality: 'General physician',
+    image: General_physician
+  },
+  {
+    speciality: 'Dermatology',
+    image: Dermatology
+  },
+  {
+    speciality: 'Radiology',
+    image: Radiology
+  },
+  {
+    speciality: 'Orthopedics',
+    image: Orthopedics
+  },
+  {
+    speciality: 'Pediatrics',
+    image: Pediatrics
+  },
+  {
+    speciality: 'Cardiology',
+    image: Cardiology
+  }
+];
