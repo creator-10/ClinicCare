77dd0ba6e26b5ef8902fdadc0b571f2143b407f2
VishnuRaj <vishnuraj10@gmail.com>
2025-07-20 11:36:18 +0530

    Add admin routes for login, doctor approval, rejection, and listing with auth checks

--- backend/routes/adminRoute.js
+++ backend/routes/adminRoute.js
@@ -4,7 +4,6 @@ import {
   getPendingDoctors,
   approveDoctor,
   rejectDoctor,
-  //  getApprovedDoctors,
   getDoctorsList
 } from '../controller/adminController.js';
 import auth from '../middlewares/authUser.js';
@@ -15,7 +14,6 @@ router.post('/login', loginAdmin);
 router.get('/doctors/pending', auth('admin'), getPendingDoctors);
 router.post('/doctors/:id/approve', auth('admin'), approveDoctor);
 router.delete('/doctors/:id', auth('admin'), rejectDoctor);
-// router.get('/doctors/list', getApprovedDoctors);
 router.get('/doctors/list', auth(),getDoctorsList);
 
 export default router;
