22895fe2a758d9baf066116f59bceb1b8ccbbdf8
VishnuRaj <vishnuraj10@gmail.com>
2025-07-20 11:55:18 +0530

    Add Static assets(Images,icons)

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,161 @@
+// src/assets/assets.js
+
+// Image Imports
+import register from './p_register.jpg';
+import General_physician from './General_physician.png';
+import logo from './logo.png';
+import profile_pic from './profile_pic.jpg'; 
+import dropdown_icon from './dropdown-icon.png';
+import arrow_icon from './arrow_icon.jpg';
+import group_profiles from './group_profiles.png';
+import header_img from './header_img.png';
+import Dermatology from './dermatology.jpg';
+import Radiology from './radiology.png';
+import Radiologys from './radiologys.png';
+import Orthopedics from './orthopedics.png';
+import Pediatrics from './pediatrics.png'; 
+import Cardiology from './cardiology.png';
+import doccardiology from './doc-cardiology.png';
+import about from './about.jpg';
+import contact from './contact.jpg';
+import cross_icon from './cross-icon.png';
+import appointment from './appointment.jpg';
+import pediatric from './pediatric.png'
+import calendar_icon from './calendar-icon.png';
+import history_icon from './history-icon.png';
+import logout_icon from './logout-icon.png';
+import profile from './profile_pic.png'; 
+import specialist from './speciality.png';
+import booknow from './booknow.png';
+import doctorimage from './doctorimage.jpg';
+import cardiologies from './cardiologies.png';
+import general_physicianicon from './general_physicianicon.png';
+import appointment1 from './appointmenth.png';
+import myappointment from './myappointment.png';
+import logout from './logout.png';
+import docortho from './docortho.png';
+import backgroundimage from './backgroundimage.jpg';
+import backImage from "./backappointment.jpg";
+import Bookapt from "./booka.png";
+import Home from "./home.png";
+import profileback from "./profileback.jpg";
+import registration from "./registration.jpg";
+import feedback1 from "./feedback.jpg";
+import alldoctors from "./alldoctors.jpg";
+import specialitybackground from "./specialitybackground.jpg";
+
+// Doctor thumbnails
+import doc from './doc.jpg';
+import doc1 from './doc1.jpg';
+import doc2 from './doc2.jpg';
+import doc3 from './doc3.jpg';
+import doc4 from './doc4.jpg';
+import doc5 from './doc5.jpg';
+import doc6 from './doc6.jpg';
+import doc7 from './doc7.jpg';
+import doc8 from './doc8.jpg';
+import doc9 from './doc9.jpg';  
+import doc10 from './doc10.jpg';
+import doc11 from './doc11.jpg';
+import doc12 from './doc12.jpg';
+
+export const doctorImages = [
+  doc1,
+  doc2,
+  doc3,
+  doc4,
+  doc5,
+  doc6,
+  doc7,
+  doc8,
+  doc9,
+  doc10,
+  doc11,
+  doc12,
+  doc,
+];
+
+export const assets = {
+  register,
+  registration,
+  appointment1,
+  myappointment,
+  General_physician,
+  backgroundimage,
+  backImage,
+  feedback1,
+  Home,
+  Bookapt,
+  docortho,
+  logo,
+  profileback,
+  doctorimage,
+  doccardiology,
+  specialitybackground,
+  cross_icon,
+  pediatric,
+  cardiologies,
+  general_physicianicon,
+  cardiology: Cardiology,
+  dermatology: Dermatology,
+  booknow,
+  logout,
+  profile_pic,
+  profile,
+  radiologys: Radiology,
+  pediatrics: Pediatrics,
+  speciality: specialist,
+  orthopedics: Orthopedics,
+  dropdown_icon,
+  arrow_icon,
+  group_profiles,
+  header_img,
+  Dermatology,
+  Radiology,
+  Radiologys,
+  Orthopedics,
+  Pediatrics,
+  Cardiology,
+  about,
+  contact,
+  alldoctors,
+  appointment,
+  calendar_icon,
+  history_icon,
+  logout_icon,
+  specialist
+
+};
+
+export const specialityData = [
+  {
+    speciality: 'General physician',
+    slug: 'general-physician',
+    image: doctorimage
+  },
+  {
+    speciality: 'Dermatology',
+    slug: 'dermatology',
+    image: Dermatology
+  },
+  {
+    speciality: 'Radiology',
+    slug: 'radiology',
+    image: Radiology
+  },
+  {
+    speciality: 'Orthopedics',
+    slug: 'orthopedics',
+    image: docortho
+  },
+  {
+    speciality: 'Pediatrics',
+    slug: 'pediatrics',
+    image: pediatric
+  },
+  {
+    speciality: 'Cardiology',
+    slug: 'cardiology',
+    image: doccardiology
+  }
+];
