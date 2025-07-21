4b559a93ff0781889b6fae8c0bdfcb8a5f948b61
Miss. Sneha Vasudevan <snehaavasudevan@gmail.com>
2025-07-21 16:09:54 +0530

    Update Home.jsx

--- frontend/src/pages/Patient/Home.jsx
+++ frontend/src/pages/Patient/Home.jsx
@@ -1,17 +1,28 @@
 import React from 'react';
-
+import { useSelector } from 'react-redux';
+import PatientSidebar from '../../components/PatientSidebar';
 import SpecialityMenu from '../../components/SpecialityMenu';
 import Scheduleappointment from '../../components/Scheduleappointment';
 
 const Home = () => {
- 
+  const { patientId } = useSelector((state) => state.auth);
 
   return (
     <div className="flex min-h-screen">
-     
+      {/* Sidebar - Left side */}
+      {patientId && (
+        <div className="hidden md:block w-64">
+          <PatientSidebar />
+        </div>
+      )}
 
-      {/* Main Content Area */}
-      <div className="flex-1 p-6">
+      {/* Main content - Right side */}
+      <div className="flex-1 p-4 md:p-6">
+        {/* On mobile, show sidebar in overlay inside PatientSidebar component */}
+        <div className="md:hidden">
+          <PatientSidebar />
+        </div>
+
         <SpecialityMenu />
         <Scheduleappointment />
       </div>
