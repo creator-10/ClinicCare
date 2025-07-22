151a176aa23052b26bd0cbe825be1b65127997de
akhilaakhi27 <96654491+akhilaakhi27@users.noreply.github.com>
2025-07-22 11:47:42 +0530

    Update DoctorBySpeciality.jsx

--- frontend/src/pages/Patient/DoctorBySpeciality.jsx
+++ frontend/src/pages/Patient/DoctorBySpeciality.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useParams, Link } from 'react-router-dom';
 import axios from 'axios';
 import { assets } from '../../assets/assets';
-import PatientSidebar from '../../components/PatientSidebar';
+
 
 const DoctorBySpeciality = () => {
   const { speciality } = useParams();
